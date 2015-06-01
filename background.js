017c59da2a98437c428b9925c0dbc2d44a9d62bf
patclittle <plittle@Patricks-MacBook-Pro-5.local>
2015-06-01 12:13:17 -0700

    Only inject once page load is complete
    Was injecting both for loading and complete — fixed

--- background.js
+++ background.js
@@ -1,4 +1,6 @@
 chrome.tabs.onUpdated.addListener(function(id,info,tab){
-	chrome.tabs.executeScript(null, {"file":"mainInjection.js"});
-	chrome.tabs.executeScript(null, {"file":"jquery-2.1.4.min.js"});
+	if(info.status=="complete"){
+		chrome.tabs.executeScript(null, {"file":"jquery-2.1.4.min.js"});
+		chrome.tabs.executeScript(null, {"file":"mainInjection.js"});
+	}
 });
