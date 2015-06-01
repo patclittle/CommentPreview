510e8a804b6525dd045071049eec16276696aa2b
patclittle <plittle@Patricks-MacBook-Pro-5.local>
2015-06-01 09:55:53 -0700

    Cleaned up code

--- mainInjection.js
+++ mainInjection.js
@@ -1,6 +1,20 @@
+//For testing if it loaded
 alert('Hello World!');
-var commentLinks = document.getElementsByClassName('comments');
-for (i=0;i<commentLinks.length;i++){
-	commentLinks[i].innerHTML = "Test";
+//List of links to comment pages
+var commentLinks;
+
+//Main method
+function main(){
+	fillCommentLinks();
+	for (i=0;i<commentLinks.length;i++){
+		commentLinks[i].innerHTML = "Test";
+	}
 }
-$('body').hide();
+
+//Gets all the links to coment pages and arranges in array
+function fillCommentLinks(){
+	commentLinks = document.getElementsByClassName('comments');
+}
+
+//Get everything going
+main();
