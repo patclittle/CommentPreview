6d07d37b0421b4482ff0ab41c235f4dc40d9b53f
patclittle <plittle@Patricks-MacBook-Pro-5.local>
2015-06-11 21:48:37 -0700

    Changed how comment object was treated
    Instead of a string, it is now a DOM object

--- mainInjection.js
+++ mainInjection.js
@@ -5,7 +5,6 @@ function main(){
 
 //Inserts the expando button into the reddit page
 function insertExpandoButton(){
-	console.log("hello");
 	//Build the HTML element
 	var button = document.createElement("a");
 	//Add class for keeping track of expanding
@@ -16,13 +15,16 @@ function insertExpandoButton(){
 		if (this.className.includes("unexpanded")){ // expand the comments
 			if (this.className.includes("opened")){ // if already opened
 				$(this).siblings(".commentContent").show(); //just show the div
-				this.className = "commentExpander opened";
 			}else{ // if not already opened
 				insertCommentDiv($(this)); // open and show comments
-				this.className = "commentExpander opened";
 			}
+			//Change around the button to be a collapse
+			this.innerText = "Collapse";
+			this.className = "commentExpander opened";
 		}else{ // unexpand comments
 			$(this).siblings(".commentContent").hide();
+			//Change button to be an expand
+			this.innerText = "Expand";
 			this.className = "commentExpander opened unexpanded";
 		}
 	});
@@ -49,16 +51,22 @@ function insertCommentDiv(theButton){
 	$(theButton).siblings(".flat-list").after(commentDiv);
 	//Get comments and write to div
 	$.getJSON(theURL,function foo(result) {
+		//Loop running through all top replies
 		$.each(result[1].data.children.slice(0, 100),
 		    function (i, post) {
-		    	commentHTML=$('<div/>').html(post.data.body_html).text();
-		    	if(post.data.replies != ""){
-		    		moreComments=document.createElement('a');
-		    		moreComments.href = "www.google.com";
+		    	commentHTML=$('<div/>').html(post.data.body_html).text(); //comment content
+		    	commentHTML=$.parseHTML(commentHTML);
+		    	if(post.data.replies != ""){ // If there are replies to this comment
+		    		moreComments=document.createElement('a'); //link to load replies
 		    		moreComments.innerText = "Load more comments...";
-		    		commentHTML+="<span>"+moreComments.outerHTML+"</span>";
+		    		$(commentHTML).append("<span>"+moreComments.outerHTML+"</span>"); //Add link to comment
+		    		$(moreComments).on("click",function(){ //Make the link load replies
+		    			$(commentHTML.firstChild).append("<div style=\"margin-left:10px;\">"+post.data.replies.data.children[0].data.body+"</div>");
+		    		});
+		    		
 		    	}
-		        $(commentDiv).append(commentHTML+'<hr/>');
+		    	$(commentHTML).append("<hr/>");
+		        $(commentDiv).append($(commentHTML)); //Add comment content to page
 		    }
 	    )
 	})
