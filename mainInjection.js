beca8d1dc01eec806e908120c751fcbf5845b7ea
patclittle <plittle@Patricks-MacBook-Pro-5.local>
2015-06-15 23:06:28 -0700

    Cleaned up code (merged to awkward methods into a better one)

--- mainInjection.js
+++ mainInjection.js
@@ -46,6 +46,11 @@ function insertCommentDiv(theButton){
 	//Build the container div
 	commentDiv = document.createElement("div");
 	$(commentDiv).addClass("commentContent");
+
+
+	$(commentDiv).append("<span/>");
+
+
 	//Add the div to the page
 	$(theButton).siblings(".flat-list").after(commentDiv);
 	//Get comments and write to div
@@ -53,60 +58,22 @@ function insertCommentDiv(theButton){
 		//Loop running through all top replies
 		$.each(result[1].data.children.slice(0, 100),
 		    function (i, post) {
-		    	commentHTML=$('<div/>').html(post.data.body_html).text(); //comment content
-		    	commentHTML=$.parseHTML(commentHTML); //Make the comment into an HTML object
-		    	$(commentHTML).prepend("<a href=\"/user/"+post.data.author+"\">"+post.data.author+"</a> "+post.data.score+" points");
-		    	$(commentHTML).addClass("commentP");
-		    	if(post.data.replies != ""){ // If there are replies to this comment
-		    		insertReplies(post.data.replies,commentHTML);	
-		    	}
-		        $(commentDiv).append($(commentHTML)); //Add comment content to page
+		        insertComment(post.data,commentDiv.lastChild);
 		    }
 	    )
 	})
 }
 
-
-/*Inserts the replies into the thread
- * @param post is the comment to load replies for
- * @param context is the html object for the comment to load replies for
+/*
+ * Inserts a specific comment in a specific place
+ * @param data is the data of the comment from the JSON
+ * @param context is the context of where to insert the comment
+ *     (comment will go directly before the context)
  */
-function insertReplies(post,context){
-	var moreComments; //HTML object for the "load more comments" button
-	var replyHTML; //HTML for the first reply
-	var theReply; //the reply to load
-	var replyNum; //the number of reply in the thread
-
-	moreComments=document.createElement('a'); //link to load replies
-	moreComments.innerText = "Load more comments...";
-	moreComments.className = "0";
-	$(moreComments).on("click",function(){ //Make the link load replies
-		console.log(post);
-		theReply = post.data.children[this.className].data;
-		replyHTML=$('<div/>').html(theReply.body_html).text(); 
-		replyHTML=$.parseHTML(replyHTML); //Build the HTML from the JSON
-		$(replyHTML).prepend("<a href=\"/user/"+theReply.author+"\">"+theReply.author+"</a> "+theReply.score+" points");
-		$(replyHTML).addClass("commentP");
-		$(this).parent().before($(replyHTML)); //insert comment
-		if (theReply.replies != ""){ // If there are replies to this reply
-			if(theReply.replies.data.children[0].kind != "more"){
-				insertReplies(theReply.replies,replyHTML); // load those replies
-			}
-		}
-		//keep track of which number reply we are at with class name
-		replyNum = (+this.className)+1;
-		if(replyNum<post.data.children.length-1){ //if there are more replies to load
-			this.className = replyNum; // get ready to load next reply
-		}else{
-			$(this).hide(); // otherwise hide the button
-		}
-	});
-	$(context).append($('<span/>').html($(moreComments)));
-}
-
 function insertComment(data,context){
 	var commentHTML; //DOM object for comment
 	var repliesLink; //Dom object for replies link
+	var replyNum; //for keeping track of how many replies have been loaded
 
 	//Set up comment
 	commentHTML=$('<div/>').html(data.body_html).text();
@@ -115,13 +82,32 @@ function insertComment(data,context){
 	$(commentHTML).addClass("commentP");
 
 	//Add "load replies" button if necessary
-	if(data.replies!=""){
-		if(data.replies.data.children[0].kind!="more"){
+	if(data.replies!=""){ //Check if there are replies
+		if(data.replies.data.children[0].kind!="more"){ //Check if the reply is already loaded
+			//Create button to load a reply
 			repliesLink=document.createElement('a');
 			repliesLink.innerText="Load more comments...";
 			repliesLink.className="0";
+
+			//Add functionality to button
+			$(repliesLink).on("click",function(){
+				insertComment(data.replies.data.children[this.className].data,repliesLink);
+				//keep track of which number reply we are at with class name
+				replyNum = (+this.className)+1;
+				if(replyNum<data.replies.data.children.length){ //if there are more replies to load
+					this.className = replyNum; // get ready to load next reply
+				}else{
+					$(this).hide(); // otherwise hide the button
+				}
+			});
+
+			//Add button to the comment
+			$(commentHTML).append(repliesLink);
 		}
 	}
+
+	//insert the comment before the context
+	$(context).before(commentHTML);
 }
 
 //Get everything going
