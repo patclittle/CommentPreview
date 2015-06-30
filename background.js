2a42be34ee2099ce881ab959d14fc8da35df26a5
patclittle <plittle@Patricks-MacBook-Pro-5.local>
2015-06-29 20:21:21 -0700

    Organized file structure

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 chrome.tabs.onUpdated.addListener(function(id,info,tab){
 	if(info.status=="complete" && !tab.url.includes("comment")){
-		chrome.tabs.executeScript(null, {"file":"jquery-2.1.4.min.js"});
-		chrome.tabs.executeScript(null, {"file":"mainInjection.js"});
-		chrome.tabs.insertCSS(null,{"file":"injectedStyle.css"});
+		chrome.tabs.executeScript(null, {"file":"mainInjection/JS/jquery-2.1.4.min.js"});
+		chrome.tabs.executeScript(null, {"file":"mainInjection/JS/mainInjection.js"});
+		chrome.tabs.insertCSS(null,{"file":"mainInjection/CSS/injectedStyle.css"});
 	}
 });
