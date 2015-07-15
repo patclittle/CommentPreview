30a2b7233da890fbd7621bafb355ae55a3e4d38f
patclittle <plittle@Patricks-MacBook-Pro-5.local>
2015-07-15 10:32:41 -0700

    Only works on reddit now

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener(function(id,info,tab){
-	if(info.status=="complete" && !tab.url.includes("comment")){
+	if(info.status=="complete" && !tab.url.includes("comment") && tab.url.includes("reddit")){
 		chrome.tabs.executeScript(null, {"file":"mainInjection/JS/jquery-2.1.4.min.js"});
 		chrome.tabs.executeScript(null, {"file":"mainInjection/JS/mainInjection.js"});
 		chrome.tabs.insertCSS(null,{"file":"mainInjection/CSS/injectedStyle.css"});
